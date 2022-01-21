63480b26b3fa51591dc239e6db5a7b964fe11f52
Rey Rodriguez <r.reydev@gmail.com>
2022-01-21 12:48:14 -0400

    basic animation to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,38 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      state(
+        'void',
+        style({
+          opacity: 0,
+        })
+      ),
+      transition('void <=> *', animate(2000)),
+    ]),
+  ],
 })
 export class HeaderComponent implements OnInit {
-
   @Input() showLogo: boolean = false;
   @Input() textButton: string = '';
   @Input() redirectTo: string = '';
-  constructor() { }
+  state = 'none';
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.state = '*';
   }
-
 }
